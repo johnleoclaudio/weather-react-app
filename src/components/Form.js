1fb252c41daf6cc5147790b003de9f4b7f560eda
John <johncleoclaudio@gmail.com>
2018-04-20 16:51:45 +0800

    Added styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ const Form = props => (
     <form onSubmit={props.getWeather}>
         <input type="text" name="city" placeholder="City"/>
         <input type="text" name="country" placeholder="Country"/>
-        <button>Get Weather</button>
+        <button className="btn btn-default">Get Weather</button>
     </form>
 );
 
